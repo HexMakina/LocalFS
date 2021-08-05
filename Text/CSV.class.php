2c37f62931259155c65dc39bb6933da03da49746
sam <sammy@hexmakina.be>
2021-08-06 01:42:24 +0200

    moved function collection_of_models_to_csv($collection, $model, $output) to ExportController

--- Text/CSV.class.php
+++ Text/CSV.class.php
@@ -4,7 +4,7 @@ namespace HexMakina\LocalFS\Text;
 
 class CSV extends TextFile
 {
-	const LIST_ANORMAL_SEPARATOR_REGEX = '/\r\n|\n|\r/';
+
 
   // T — Gets line from file pointer and parse for CSV fields
   // fgetcsv ( resource $handle [, int $length = 0 [, string $delimiter = "," [, string $enclosure = '"' [, string $escape = "\\" ]]]] ) : array
@@ -34,40 +34,5 @@ fclose($fp);
 		return array_map('str_getcsv', parent::array());
 	}
 
-  //TODO WTF is this doing here ??
-  public static function collection_of_models_to_csv($collection, $model, $output)
-  {
-    if($output === 'php://output')
-    {
-      $stream = "$output";
-      header('Content-Description: File Transfer');
-      header('Content-Type: application/octet-stream');
-      header('Content-Disposition: attachment; filename=export_'.date('Ymd_Hi').".csv");
-      header('Content-Transfer-Encoding: binary');
-      header('Expires: 0');
-      header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
-      header('Pragma: public');
-    }
-    else
-    {
-      $stream = $output;
-    }
-
-    $stream = fopen($stream, "w");
-    // csv header
-    fputcsv($stream, array_keys($model->fields())); // CSV headers
-
-
-    foreach($collection as $model)
-    {
-      $model = $model->export();
-      foreach($model as $key => $value){
-        $model[$key] = preg_replace(Model::LIST_ANORMAL_SEPARATOR_REGEX, '|', $value);
-      }
-      fputcsv($stream, $model);
-    }
-    fclose($stream);
-    exit();
-  }
 
 }
