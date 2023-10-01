3f4cd8a296543ce6b462a085c8ef752bf104f3be
HexMakina <20891495+HexMakina@users.noreply.github.com>
2023-10-01 17:59:47 +0200

    cleanup

--- FileSystem.php
+++ FileSystem.php
@@ -130,19 +130,20 @@ class FileSystem
      * @return bool True if the path is writable, false otherwise.
      */
 
-    public function ensureWritablePath(string $absolutePath): bool
+    public function ensureWritablePath(string $absoluteDirectoryPath, string $filename=null): bool
     {
-        if (strpos($absolutePath, $this->root()) !== 0) {
+        if (strpos($absoluteDirectoryPath, $this->root()) !== 0) {
             throw new \InvalidArgumentException('PATH_NOT_INSIDE_ROOT_PATH');
         }
 
-        $relativePath = substr($absolutePath, strlen($this->root()) + 1);
-
-        $pathParts = explode('/', pathinfo($relativePath, PATHINFO_DIRNAME));
+        $relativeDirectoryPath = substr($absoluteDirectoryPath, strlen($this->root()) + 1);
+        $pathParts = explode('/', $relativeDirectoryPath);
+        
 
         $targetDir = $this->root();
         foreach ($pathParts as $i => $part) {
             $targetDir .= '/' . $part;
+
             // Create the folder if it doesn't exist
             if (!file_exists($targetDir) && mkdir($targetDir, 0755, true) === false) {
                 throw new \InvalidArgumentException('UNABLE_TO_CREATE_MISSING_TARGET_DIRECTORY');
@@ -151,10 +152,9 @@ class FileSystem
             if (!is_dir($targetDir)) {
                 throw new \InvalidArgumentException('TARGET_DIRECTORY_NOT_A_DIRECTORY');
             }
-            // // TODO test writable only on last dir ?
-            // if (!is_writable($targetDir)) {
-            //     throw new \InvalidArgumentException('TARGET_DIRECTORY_NOT_WRITABLE');
-            // }
+            if (!is_writable($targetDir)) {
+                throw new \InvalidArgumentException('TARGET_DIRECTORY_NOT_WRITABLE');
+            }
         }
         return true;
     }
@@ -201,50 +201,86 @@ class FileSystem
         }
         return $path;
     }
-
+    /**
+     * Copies a file from the source path to the destination path.
+     *
+     * @param string $sourcePath The path of the source file.
+     * @param string $destinationPath The path of the destination file.
+     * @return bool Returns TRUE on success or FALSE on failure.
+     */
     public static function copy($sourcePath, $destinationPath)
     {
         if (file_exists($sourcePath) && is_file($sourcePath)) {
             $destination = new FilePath($destinationPath);
             if (file_exists($destination->dir()) && is_dir($destination->dir())) {
-                return copy($sourcePath, $destinationPath); // Returns TRUE on success or FALSE on failure.
+                return copy($sourcePath, $destinationPath);
             }
         }
         return false;
     }
 
+    /**
+     * Moves a file from the source path to the destination path.
+     *
+     * @param string $sourcePath The path of the source file.
+     * @param string $destinationPath The path of the destination file.
+     * @return bool Returns TRUE on success or FALSE on failure.
+     */
     public static function move($sourcePath, $destinationPath)
     {
         if (file_exists($sourcePath) && is_file($sourcePath)) {
             $destination = new FilePath($destinationPath);
             if (file_exists($destination->dir()) && is_dir($destination->dir())) {
-                return rename($sourcePath, $destinationPath); // Returns TRUE on success or FALSE on failure.
+                return rename($sourcePath, $destinationPath);
             }
         }
 
         return false;
     }
 
+    /**
+     * Creates a directory with the specified path and permissions.
+     *
+     * @param string $directoryPath The path of the directory to create.
+     * @param int $permission The permissions to set for the directory.
+     * @param bool $recursive Whether to create parent directories if they don't exist.
+     * @return bool Returns TRUE on success or FALSE on failure.
+     */
     public static function makeDirectory($directoryPath, $permission = 0777, $recursive = true): bool
     {
         return mkdir($directoryPath, $permission, $recursive);
     }
 
-    public static function remove($sourcePath, $followLink = false)
+    /**
+     * Removes a file or directory with the specified path.
+     *
+     * @param string $sourcePath The path of the file or directory to remove.
+     * @param bool $followLink Whether to follow symbolic links.
+     * @return bool Returns TRUE on success or FALSE on failure.
+     */
+    public static function remove($sourcePath, $followLink = false): bool
     {
         $success = false;
         if (file_exists($sourcePath)) {
             if (is_link($sourcePath) && $followLink === true) {
                 $success = self::remove(readlink($sourcePath));
             } elseif (is_file($sourcePath)) {
-                $success = unlink($sourcePath); // Returns TRUE on success or FALSE on failure.
+                $success = unlink($sourcePath);
             } elseif (is_dir($sourcePath)) {
-                $success = rmdir($sourcePath); // Returns TRUE on success or FALSE on failure.
+                $success = rmdir($sourcePath);
             }
         }
         return $success;
     }
 
+    /**
+     * Scans a directory for files and directories that match a regular expression.
+     *
+     * @param string $directoryPath The path of the directory to scan.
+     * @param string|null $regex The regular expression to match against file and directory names.
+     * @return array|null Returns an array of file and directory names that match the regular expression, or NULL if the directory doesn't exist.
+     * @throws \Exception If the directory cannot be scanned.
+     */
     public static function pregScandir($directoryPath, $regex = null)
     {
         if (!file_exists($directoryPath) || !is_dir($directoryPath)) {
@@ -256,22 +292,5 @@ class FileSystem
         }
 
         throw new \Exception("directory path '$directoryPath' cannot be scanned");
-    }
-
-    private function validRelativePath(string $relativePath): bool
-    {
-        if (empty($relativePath)) {
-            throw new \InvalidArgumentException('EMPTY_RELATIVE_PATH');
-        }
-
-        if (strpos($relativePath, '/') === 0) {
-            throw new \InvalidArgumentException('RELATIVE_PATH_CANNOT_START_WITH_SLASH');
-        }
-
-        if (strpos($relativePath, '..') !== false) {
-            throw new \InvalidArgumentException('RELATIVE_PATH_CANNOT_CONTAIN_PARENT_DIRECTORY');
-        }
-
-        return true;
     }
 }
